600a974f835c40b6f5cedb490614237c356d1efa
laurasofiaparracruz-byte <laurasofiaparracruz@gmail.com>
2025-11-06 18:29:05 +0000

    avance java

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-const titulo = document.getElementById("titulo")
+const titulo = document.getElementById("titulo");
+const textoInicial = document.getElementById("textoInicial");
+let estadoInicial = true;
+let posicionTexto = 0;
+const textos = ["hola", "Bienvenides", "a", "esta", "página", "web"];
+
+textoInicial.innerText = textos[0];
+
+textoInicial.addEventListener("click", () => {
+   if (posicionTexto < textos.length - 1) {
+   posicionTexto = posicionTexto+1;
+   textoInicial.innerText = textos[posicionTexto]
+}else {posicionTexto = 0
+   textoInicial.innerText = textos [posicionTexto];}
+})
 
 titulo.addEventListener("click", ()=> {
-   titulo.innerText = "Bienvenides";
+   if(estadoInicial === true) {
+      titulo.innerText = "Bienvenides";
+   }
+   else {titulo.innerText = "hola"; }
+      estadoInicial=!estadoInicial;
 })
